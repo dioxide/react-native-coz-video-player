cfdbd83c16a96f678ee2c63b0843c691982452ef
dioxide <tykingco2@gmail.com>
2017-12-27 22:02:24 +0800

    fix js

--- library/Controls.js
+++ library/Controls.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from "prop-types";
 
 import ReactNative, {
   StyleSheet,
@@ -14,7 +15,7 @@ import ReactNative, {
   ActivityIndicator,
 } from 'react-native';
 
-import Slider from '@ldn0x7dc/react-native-slider';
+import Slider from './Slider';
 
 /**
  * format as --:-- or --:--:--
