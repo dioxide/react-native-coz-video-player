cf2b607b35707ddeac564335aa48d2fd50ced481
dioxide <tykingco2@gmail.com>
2018-01-07 03:51:24 +0800

    update

---
+++ android/src/main/java/com/coz/rn/media/videoplayer/MediaKitPackage.java
@@ -0,0 +1,25 @@
+package com.coz.rn.media.videoplayer;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.greatdroid.reactnative.media.player.ReactMediaPlayerViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MediaKitPackage implements ReactPackage {
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Arrays.<ViewManager>asList(new ReactMediaPlayerViewManager());
+  }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import VideoPlayerBasic from './player/basic'
+import VideoPlayerNormal from './player/normal'
+
+export {VideoPlayerBasic, VideoPlayerNormal}
