3a4551b1105e9e0ad5d91d25ffd641cb7d58638f
dioxide <tykingco2@gmail.com>
2018-01-07 04:03:35 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import VideoPlayerBasic from './player/basic'
-import VideoPlayerNormal from './player/normal'
+import {Video as VideoPlayerBasic} from './player/basic'
+import {VideoPlayer as VideoPlayerNormal} from './player/normal'
 
 export {VideoPlayerBasic, VideoPlayerNormal}
